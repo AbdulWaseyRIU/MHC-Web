d7e84ac8fb0a4b08ce9689df4a48d5b01b705fdb
Maternity Health Care System <24781@students.riphah.edu.pk>
2023-11-11 19:15:21 +0500

    login

--- resources/views/Layout/header.blade.php
+++ resources/views/Layout/header.blade.php
@@ -3,7 +3,7 @@
     <header>
         <div class="slider-container">
             <img src="{{ asset('graphics/slider.jpg')}}" alt="">
-            <div class="slider-text">
+         <div class="slider-text">
                 <h2>Welcome to Maternity Health Care System</h2>
                 <p>Your trusted partner for child healthcare.</p>
             </div>
@@ -11,9 +11,9 @@
         <div class="top-bar">
             <div class="logo">Maternity Health Care System</div>
             <nav class="nav">
-                <a href="#">Home</a>
+                <a href="/">Home</a>
                 <a href="#">About our Software</a>
-                <a href="#">How to Scan</a>
+                <a href="scan">Scan</a>
                 <a href="#">Contact</a>
             </nav>
         </div>
@@ -23,9 +23,9 @@
             <h2 class="centered-text h2">Our Services</h2>
         </div>
         <div class="box-row">
-      
+
             <div class="box-item">Gender Detection</div>
             <div class="box-item">Fetus Growth</div>
         </div>
-    
+
     </div>
