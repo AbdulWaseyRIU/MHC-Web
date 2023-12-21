18d49c9d011b4679772b9048d5be7d537d3cf134
Maternity Health Care System <24781@students.riphah.edu.pk>
2023-12-21 20:12:38 +0500

    design improved

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,12 +1,42 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
-
+@extends('Layout.app')
+@section('content')
+<main >
+    <div class="container">
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <form method="POST" action="{{ route('login') }}">
+        @csrf <h2>Login First</h2>
 
-    <form method="POST" action="{{ route('password.email') }}">
+        <!-- Email Address -->
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input id="email" type="email" name="email" value="{{ old('email') }}" required autofocus autocomplete="username" />
+        </div>
+
+        <!-- Password -->
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input id="password" type="password" name="password" required autocomplete="current-password" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="remember-me">
+            <input id="remember_me" type="checkbox" name="remember">
+            <label for="remember_me">Remember me</label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a href="{{ route('password.request') }}" class="forgot-password">Forgot your password?</a>
+            @endif
+
+            <a href="scan" class="login-button">Log in</a>
+        </div>  <!-- Registration Link -->
+        <div class="registration-link">
+         <a href="{{ route('register') }}">Don't have an account? Register now!</a>
+     </div>
+    </form>
+    <form method="POST" action="/">
         @csrf
 
         <!-- Email Address -->
@@ -22,4 +52,7 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+    </div>
+</main>
+
+@endsection
